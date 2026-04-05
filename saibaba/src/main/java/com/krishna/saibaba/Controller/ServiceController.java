ec653bb4ab1cee76bc879de949949da61d6999dc
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-04-05 15:12:11 +0530

    Update ServiceController.java

--- saibaba/src/main/java/com/krishna/saibaba/Controller/ServiceController.java
+++ saibaba/src/main/java/com/krishna/saibaba/Controller/ServiceController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.util.List;
 import java.io.File;
 
 @RestController
